a7e8ff62e70907f93f970a04bf3ca54cb5164895
aditibansal1904-web <aditi.bansal1904@gmail.com>
2026-02-18 18:56:39 +0530

    Add files via upload

---
+++ assignemnt 1.py
@@ -0,0 +1,29 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> num1 = 20
+>>> num2 = 10
+>>> 
+>>> num1+num2
+30
+>>> 
+>>> num1-num2
+10
+>>> 
+>>> num1*num2
+200
+>>> 
+>>> num1/num2
+2.0
+>>> 
+>>> num1//num2
+2
+>>> 
+>>> first_name = "alex"
+>>> last_name = "jones"
+>>> full_name = first_name + " " + last_name
+>>> 
+>>> full_name
+'alex jones'
+>>> 
+>>> print("hello", full_name, "it's great to see you")
+hello alex jones it's great to see you
